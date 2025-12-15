3ec1c206946b8925a1def14e5fe23bbe92c31a40
solgithub1 <Sallybd440@gmail.com>
2025-12-15 14:04:26 +0200

    title

--- src/view/PlayersNamesScreen.java
+++ src/view/PlayersNamesScreen.java
@@ -20,7 +20,7 @@ public class PlayersNamesScreen extends JFrame {
         this.levelScreen = levelScreen;
         this.homeScreen = homeScreen;
 
-        setTitle("MineSweepesr_Players");
+        setTitle("MineSweepesrPlayers");
         setSize(800, 550);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
